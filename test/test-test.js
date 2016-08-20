572a763e2ae0bb1acb36164fb4e1b3bb998df55b
kobzarvs <kobzarvs@gmail.com>
2016-08-20 20:01:03 +0300

    Добавил test karma.conf.js

---
+++ test/test-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+//import Foo from '../src/Foo';
+
+describe("A suite", function () {
+    it("contains spec with an expectation", function() {
+       expect(1).toBe(0);
+    });
+});
